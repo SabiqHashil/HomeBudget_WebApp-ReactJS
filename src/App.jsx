ccb7b2cf75daf58f1441eb2754ff4c984112ccd0
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2023-08-19 02:08:04 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+// Routes
+import Dashboard, { dashboardLoader } from "./pages/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+    loader: dashboardLoader,
+  },
+  {
+    path: "/about",
+    element: <h1>About</h1>,
+  },
+]);
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
