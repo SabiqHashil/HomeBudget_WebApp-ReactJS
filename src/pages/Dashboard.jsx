1b7734f87ad7723fb5995a430632065d1735789f
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2023-08-19 20:36:46 +0530

    update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,21 +4,36 @@ import { useLoaderData } from "react-router-dom";
 // helper functions
 import { fetchData } from "../helpers";
 
+// components
+import Intro from "../components/Intro";
+
+// library imports
+import { toast } from "react-toastify";
+import Error from "./Error";
+
 // loader
 export function dashboardLoader() {
   const userName = fetchData("userName");
   return { userName };
 }
 
+//action
+export async function dashboardAction({ request }) {
+  const data = await request.formData();
+  const formData = Object.fromEntries(data);
+  try {
+    throw new Error("Ya done");
+    localStorage.setItem("userName", JSON.stringify(formData.userName));
+    return toast.success(`Welcome, ${formData.userName}`);
+  } catch (e) {
+    throw new Error("There was a problem creating your account.");
+  }
+}
+
 const Dashboard = () => {
   const { userName } = useLoaderData();
 
-  return (
-    <div>
-      <h1>{userName} </h1>
-      Dashboard
-    </div>
-  );
+  return <div>{userName ? <p>{userName}</p> : <Intro />}</div>;
 };
 
 export default Dashboard;
