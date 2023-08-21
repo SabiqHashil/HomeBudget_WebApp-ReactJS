6b6e9aaa723bd6e95f6441be6f9f56bee585efb0
Sabiq Hashil <sabiqhashilkp786@gmail.com>
2023-08-22 00:18:49 +0530

    update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,6 +6,7 @@ import { fetchData } from "../helpers";
 
 // components
 import Intro from "../components/Intro";
+import AddBudgetForm from "../components/AddBudgetForm";
 
 // library imports
 import { toast } from "react-toastify";
@@ -14,7 +15,9 @@ import Error from "./Error";
 // loader
 export function dashboardLoader() {
   const userName = fetchData("userName");
-  return { userName };
+  const budgets = fetchData("budgets");
+
+  return { userName, budgets };
 }
 
 //action
@@ -30,9 +33,29 @@ export async function dashboardAction({ request }) {
 }
 
 const Dashboard = () => {
-  const { userName } = useLoaderData();
+  const { userName, budgets } = useLoaderData();
 
-  return <div>{userName ? <p>{userName}</p> : <Intro />}</div>;
+  return (
+    <>
+      {userName ? (
+        <div className="dashboard">
+          <h1>
+            Welcome Back,<span className="accent">{userName}</span>{" "}
+          </h1>
+          <div className="grid-sm">
+            {/* {budgets ? () : ()} */}
+            <div className="grid-lg">
+              <div className="flex-lg">
+                <AddBudgetForm />
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <Intro />
+      )}
+    </>
+  );
 };
 
 export default Dashboard;
